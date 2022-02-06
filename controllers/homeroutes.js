47d19b522d5cb2d7464abab0b64dd2f0bb8ebf47
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-05 21:33:23 -0700

    adding get route for clicking a post on hgome page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-
 const { User, Post, Comment } = require('../models');
+
+// this will display all on home without showing comments but will show username of poster
 
 router.get('/', async (req, res) => {
     // find all categories
@@ -21,5 +22,33 @@ router.get('/', async (req, res) => {
       // be sure to include its associated Products
     }
   });
+
+// this is for when you click on a post and it shows the comments on it and username of the poster and username of commenters
+router.get("/:id", async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ["body"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        { model: User, attributes: ["username"] },
+      ],
+    });
+
+    const post = dbPostData.get({ plain: true });
+    req.session.post_id = post.id
+    res.json(post)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
