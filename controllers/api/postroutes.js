ba0c5fe4d045652c78c74a9dce105fba82c5257f
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-06 00:48:18 -0700

    adding delete posts route

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -53,6 +53,24 @@ router.put('/dashboard/:id', withAuth, async (req, res) => {
     }
   });
 
+  router.delete('/delete/:id', async (req, res) => {
+    try {
+      const deletePost = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id
+        },
+      });
+      if (!deletePost) {
+        res.status(404).json({ message: 'No post found' });
+        return;
+      }
+      res.status(200).json(`Posts number ${req.params.id} has been deleted`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 
 module.exports = router;
