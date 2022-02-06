d953cca86b8683f1ce5125e7472e45c1bccb418e
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-06 00:19:31 -0700

    adding commenting route

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const { Post, Comment, User} = require('../../models');
+const withAuth = require('../../utils/auth')
 
-router.get('/dashboard/:id', async (req, res) =>{
+router.get('/dashboard/:id', withAuth, async (req, res) =>{
     try {
         const usersPost = await Post.findByPk(req.params.id );
         const post = usersPost.get({ plain: true });
         req.session.post_id = post.id;
         res.json(post);
       } catch (err) {
+        res.status(400).json("its something else");
+      }
+})
+
+router.post('/comment', withAuth, async (req, res) => {
+    try {
+
+        const userComment = await Comment.create({
+          body: req.body.body,
+          user_id: req.session.user_id,
+          post_id: req.session.post_id,
+        });
+        res.status(200).json(userComment);
+    } catch(err) {
         res.status(400).json("its something else");
       }
 })
