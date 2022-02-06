523ee243df918ee472b90934cd77e711fffe69a0
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-05 19:18:38 -0700

    adding homeroute to display post

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 
+const { User, Post, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    // find all categories
+    try {
+      const category = await Post.findAll();
+      res.json(category);
+    } catch (err) {
+      console.log("Wrong");
+      // be sure to include its associated Products
+    }
+  });
+
 module.exports = router;
