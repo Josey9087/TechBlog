b1af1d3631156472681704f5c2b7ccd6325996e0
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-05 19:49:14 -0700

    adding username output to get route for post:

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -5,7 +5,16 @@ const { User, Post, Comment } = require('../models');
 router.get('/', async (req, res) => {
     // find all categories
     try {
-      const category = await Post.findAll();
+      const category = await Post.findAll(
+        {
+            include: [
+              {
+                model: User,
+                attributes: ['username'],
+              },
+            ],
+          }
+      );
       res.json(category);
     } catch (err) {
       console.log("Wrong");
