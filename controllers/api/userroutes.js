404a26fe2b5c815b9fd524a2b231689236992ba4
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-05 20:38:33 -0700

    adding post route in username to create new user

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -14,4 +14,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+    if (userData) {
+      res
+        .status(400)
+        .json({ message: 'User already exists please login' });
+      return;
+    }
+    const usercreateData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = usercreateData.id;
+      req.session.logged_in = true;
+      res.json({ user: req.session.user_id, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
